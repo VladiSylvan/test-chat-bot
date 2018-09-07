7f624ea5f29f6efa6da47b3b7160d0133afc5377
Vladimir <Selynin>
2018-09-07 18:23:48 +0300

    add save to local storage and render frpm ls

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,17 @@
          ref="text"
          contenteditable="true">
     </div>
-    <div v-if="showMenu"
+    <div ref="menu"
+         @keyup="showSelect($event)"
+         v-show="showMenu"
          class="item-list"
          :style="{'left': dropDownCoordinates.left + 'px', 'top': dropDownCoordinates.top + 'px'}"
          contenteditable="false">
-      <ul v-for="item in info"
-          :key="item.index">
-        <li v-for="el in item"
-            :key="el.id"
-            :class="currentTarget == el ? 'lister' : (info['system'] === item ? 'system' : 'list')"
-            @click="addText(item, el)">{{el}}
+      <ul>
+        <li v-for="item in data"
+            :key="item.index"
+            :class="currentTarget == item.el ? 'lister' : item.type"
+            @click="addText(item.type, item.el)">{{item.el}}
         </li>
       </ul>
     </div>
@@ -29,7 +30,7 @@
 export default {
   name: 'app',
   data: () => ({
-    text: `asdasdasd asdas das`,
+    text: '',
     info: {'system': ['email', 'first name'],  'list': ['gender2', 'age']},
     showMenu: false,
     dropDownCoordinates: {left: 0, top: 0},
@@ -38,23 +39,39 @@ export default {
     data: []
   }),
   created () {
-        for (var el in this.info) {
-          this.info[el].forEach(e => {
-             this.data.push({type: el, el: e})
-          })
-        }
+    for (var el in this.info) {
+      this.info[el].forEach(e => {
+      this.data.push({type: el, el: e})
+      })
+    }
+  },
+  mounted () {
+    if (window.localStorage.text) {
+      this.$refs.text.innerText = window.localStorage.text
+      let newText = this.$refs.text.innerHTML
+      let regex = /({{[a-zA-Z0-9]+}}|{{\S*\s+(\S+)}})/g
+      let item = newText.match(regex);
+      item.forEach((el) => {
+        let result = this.data.filter(obj => {
+        return obj.el == el.slice(2, -2)
+        })
+        let newItem = '&nbsp<span contenteditable="false" class="' + result[0].type + '">' + el + '</span>&nbsp;'
+        let regex1 =  new RegExp('\([^>]' + el + ')|(^' + el + ')' );
+        this.$refs.text.innerHTML = this.$refs.text.innerHTML.replace(regex1, newItem)
+      })
+    }
   },
   methods: {
+    getKeyByValue(object, value) {
+      return Object.keys(object).find(key => object[key] === value);
+    },
     getCaretPosition(editableDiv) {
-      var caretPos = 0,
-          sel, range;
+      var sel = 0,
+          range;
       if (window.getSelection) {
         sel = window.getSelection();
         if (sel.rangeCount) {
           range = sel.getRangeAt(0);
-          if (range.commonAncestorContainer.parentNode == editableDiv) {
-            caretPos = range.endOffset;
-          }
         }
       } else if (document.selection && document.selection.createRange) {
         range = document.selection.createRange();
@@ -64,46 +81,65 @@ export default {
           var tempRange = range.duplicate();
           tempRange.moveToElementText(tempEl);
           tempRange.setEndPoint("EndToEnd", range);
-          caretPos = tempRange.text.length;
         }
       }
       this.dropDownCoordinates.left = this.$refs.text.offsetLeft + range.endOffset * 7
       this.dropDownCoordinates.top = this.$refs.text.offsetTop + (this.$refs.text.children.length > 0 ? this.$refs.text.children.length * 25 : 25)
     },
+    placeCaretAtEnd(el) {
+      el.focus();
+      if (typeof window.getSelection != "undefined" && typeof document.createRange != "undefined") {
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        range.collapse(false);
+ 
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+
+
+      } else if (typeof document.body.createTextRange != "undefined") {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.collapse(false);
+        textRange.select();
+      }
+    },
     showSelect(evt) {
       if (/{{\B/g.test(this.$refs.text.textContent) ) {
-        if (evt.key === "ArrowUp") {
-        
-        this.currentTargetId -= 1
-        if (this.currentTargetId < 0) {
-          this.currentTargetId = this.data.length - 1
-        }
-        this.currentTarget = this.data[this.currentTargetId].el
-      }
-      if (evt.key === "ArrowDown") {
-        this.currentTargetId += 1
-        if (this.currentTargetId > this.data.length - 1) {
-          this.currentTargetId = 0
-        }
-        this.currentTarget = this.data[this.currentTargetId].el
-      }
-      if (evt.key === "Enter") {
-        this.addText(this.data[this.currentTargetId].type, this.data[this.currentTargetId].el)
-      }
         this.showMenu = true
-      } else {
+        if (evt.key === "ArrowUp") {
+          this.placeCaretAtEnd(this.$refs.text)
+          this.currentTargetId -= 1
+          if (this.currentTargetId < 0) {
+            this.currentTargetId = this.data.length - 1
+          }
+        this.currentTarget = this.data[this.currentTargetId].el
+        }
+        if (evt.key === "ArrowDown") {
+          this.currentTargetId += 1
+          if (this.currentTargetId > this.data.length - 1) {
+            this.currentTargetId = 0
+          }
+        this.currentTarget = this.data[this.currentTargetId].el
+        }
+        if (evt.key === "Enter") {
+          this.addText(this.data[this.currentTargetId].type, this.data[this.currentTargetId].el)
+        }      
+        } else {
         this.showMenu = false
       }
 
       this.getCaretPosition(this.$refs.text)
     },
     submit() {
-      this.axios.post('', this.$refs.text.innerText)
+      window.localStorage.setItem('text', this.$refs.text.innerText)
     },
     addText(item, el) {
       this.showMenu = false
-      const newItem = '<span contenteditable="false" class="' + (this.info['system'] === item ? 'system' : 'list') + '">' +  '{{' + el + '}}' + '</span>&nbsp;'
+      const newItem = '<span contenteditable="false" class="' + item + '">' +  '{{' + el + '}}' + '</span>&nbsp;'
       this.$refs.text.innerHTML = this.$refs.text.innerHTML.replace(/{{\B/g, newItem)
+      this.placeCaretAtEnd(this.$refs.text)
     },
   }
 }
@@ -140,7 +176,6 @@ export default {
   color: white;
   font-weight: 700;
   font-size: 13px;
-  text-transform: capitalize;
 }
 .list {
   background-color: #42a5f5;
@@ -149,7 +184,6 @@ export default {
   color: white;
   font-weight: 700;
   font-size: 13px;
-  text-transform: capitalize;
 }
 .lister {
   background-color: #f2f5f5;
@@ -158,7 +192,6 @@ export default {
   color: white;
   font-weight: 700;
   font-size: 13px;
-  text-transform: capitalize;
 }
 .item-list {
   background-color: white;
@@ -204,6 +237,6 @@ li:after {
   color: white;
   font-weight: 700;
   font-size: 13px;
-  text-transform: capitalize;
+  text-transform: uppercase;
 }
 </style>
